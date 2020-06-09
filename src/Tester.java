02f4933306b3c6ce8ff98cfc2a2c66176d882fbd
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-06-09 10:00:12 +0200

    Pure stream without List

--- src/Tester.java
+++ src/Tester.java
@@ -6,14 +6,11 @@ public class Tester {
 
     public static void main(String[] args) {
 
-        List<Integer> numberCollection = Stream.iterate(8, x -> x + 1)
+        Stream.iterate(8, x -> x + 1)
                 .filter(x -> x > 100)
                 .filter(x -> x % 5 == 0)
                 .map(x -> x * 3)
                 .limit(10)
-                .collect(Collectors.toList());
-
-        System.out.println("First 10 elements of Stream:");
-        System.out.println(numberCollection);
+                .forEach(System.out::println);
     }
 }
